41323a8f824a56a1f715edc98c3e4c56f47194e6
AiPrizrak <nnwarts@gmail.com>
2018-09-01 16:24:16 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(__dirname + '/veiws/header');
+
+app.set('view engine', 'hbs');
+app.use((req,res,next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log('Unable to append server log');
+        }
+    });
+    console.log(log);
+    next();
+});
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req,res) => {
+    // res.send('Hello Express!');
+    res.render('home.hbs', {
+        pageTitle: 'Hello!',
+        welcomeMessage: 'Welcome to brand new server of a brand new node user',
+        });
+});
+app.use((req,res,next) => {
+    // res.send('Hello Express!');
+    res.render('maintaines.hbs', {
+        pageTitle: 'Repair works',
+        //welcomeMessage: 'Welcome to brand new server of a brand new node user',
+        });
+});
+app.get('/about', (req, res) =>{
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        });
+
+});
+app.get('/bad', (req,res) => {
+    res.send({
+        errorMessage: 'You are trying to enter unexisting page'
+        
+    });
+});
+
+app.listen(3000, () => console.log('App is up on port 3000'));
